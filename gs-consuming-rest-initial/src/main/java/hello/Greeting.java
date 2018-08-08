9861367ae4ec413d4dbef3cf37df0e1f0d450115
Alassane Niang <adniang75@gmail.com>
2018-08-08 20:03:15 +0200

    Building a hello world RESTful Web Service

---
+++ gs-consuming-rest-initial/src/main/java/hello/Greeting.java
@@ -0,0 +1,24 @@
+package hello;
+
+public class Greeting {
+
+    private final Long   id;
+    private final String content;
+
+    public Greeting( Long id, String content ) {
+        super();
+        this.id = id;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+    
+    
+
+}
